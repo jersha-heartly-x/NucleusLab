3a175f1e0a502327ee87bd7dd18ee675a26d3b8f
JERSHA2712 <76258267+JERSHA20PW13@users.noreply.github.com>
2022-09-20 17:36:08 +0530

    Hmm

--- middlewares/getcookie.js
+++ middlewares/getcookie.js
@@ -10,7 +10,7 @@ exports.getCookie = (req, res, next) => {
         url: 'https://nucleus.amcspsgtech.in/server',
         headers: {
             'path': '/profile',
-            'cookie': student
+            'cookie': teacher
         }
     };
 
