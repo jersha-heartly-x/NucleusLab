d2c9830d9610505df8152ff170ef83fed1bab527
Jersha Heartly X <20pw13@psgtech.ac.in>
2023-02-08 22:37:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const schedule = require("./routes/schedule");
 const login_request = require("./routes/login_request");
 const login_request_admin = require("./routes/login_request_admin");
@@ -18,6 +19,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
