bce851807bb86db884433b6ebc037352e3aa7c1a
dhanu1220 <ndhanusri2003@gmail.com>
2023-08-13 13:39:25 +0530

    mysql tables

--- tables.sql
+++ tables.sql
@@ -101,3 +101,59 @@ CREATE TABLE wifi (
 );
 
 
+CREATE TABLE `device_master` (
+  `model` varchar(50) DEFAULT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Working',
+  `invoiceno` varchar(50) NOT NULL,
+  `invoicedate` date NOT NULL,
+  `location` varchar(10) NOT NULL DEFAULT 'Store',
+  `devicetype` varchar(30) NOT NULL,
+  `mac` varchar(20) DEFAULT NULL,
+  `ram` varchar(4) DEFAULT NULL,
+  `serialno` varchar(50) NOT NULL,
+  `specification` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`serialno`)
+);
+
+CREATE TABLE `computer_master`(
+  `location` varchar(10) DEFAULT NULL,
+  `mousesno` varchar(50) DEFAULT NULL,
+  `keyboardsno` varchar(50) DEFAULT NULL,
+  `monitorsno` varchar(50) DEFAULT NULL,
+  `cpusno` varchar(50) DEFAULT NULL,
+  `systemno` int DEFAULT NULL,
+  KEY `monitorsno` (`monitorsno`),
+  KEY `mousesno` (`mousesno`),
+  KEY `keyboardsno` (`keyboardsno`),
+  KEY `cpusno` (`cpusno`),
+  CONSTRAINT `computer_master_ibfk_1` FOREIGN KEY (`monitorsno`) REFERENCES `device_master` (`serialno`),
+  CONSTRAINT `computer_master_ibfk_2` FOREIGN KEY (`mousesno`) REFERENCES `device_master` (`serialno`),
+  CONSTRAINT `computer_master_ibfk_3` FOREIGN KEY (`keyboardsno`) REFERENCES `device_master` (`serialno`),
+  CONSTRAINT `computer_master_ibfk_4` FOREIGN KEY (`cpusno`) REFERENCES `device_master` (`serialno`)
+);
+
+CREATE TABLE  `dump` (
+  `dumpid` int NOT NULL AUTO_INCREMENT,
+  `disposaldate` varchar(10) DEFAULT NULL,
+  `verify` varchar(15) DEFAULT 'not verified',
+  `serialno` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`dumpid`),
+  KEY `serialno` (`serialno`),
+  CONSTRAINT `dump_ibfk_1` FOREIGN KEY (`serialno`) REFERENCES `device_master` (`serialno`)
+) ;
+
+CREATE TABLE `location` (
+  `lab` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE `device` (
+  `devicetype` varchar(20) DEFAULT NULL
+);
+
+SELECT DATE_FORMAT(disposaldate, '%d/%m/%Y') AS disposaldate_formatted FROM dump;
+ALTER TABLE dump MODIFY disposaldate VARCHAR(10);
+UPDATE dump SET disposaldate = DATE_FORMAT(str_to_date(disposaldate, '%Y-%m-%d'), '%d/%m/%Y');
+
+SELECT DATE_FORMAT(invoicedate, '%d/%m/%Y') AS invoicedate_formatted FROM device_master;
+ALTER TABLE device_master MODIFY invoicedate VARCHAR(10);
+UPDATE device_master SET invoicedate = DATE_FORMAT(str_to_date(invoicedate, '%Y-%m-%d'), '%d/%m/%Y');
