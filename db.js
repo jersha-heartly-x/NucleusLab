37f6e3a8480798e4728d8c89f5c4a5e4202d3f8f
JERSHA2712 <76258267+JERSHA20PW13@users.noreply.github.com>
2024-04-17 11:36:49 +0530

    handle db connection error

--- db.js
+++ db.js
@@ -1,21 +1,34 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const con = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  port: process.env.PORT,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-});
+let con;
 
-con.connect(function (err) {
-  try {
-    if (err) throw err;
-    console.log("Connected to database");
-  } catch (error) {
-    console.log(error);
-  }
-});
+function connectToDatabase() {
+  con = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    port: process.env.PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+  });
+
+  con.connect(function (err) {
+    if (err) {
+      console.error("Error connecting to database:", err.message);
+      console.log("Attempting to reconnect...");
+      connectToDatabase();
+    } else {
+      console.log("Connected to database");
+    }
+  });
+
+  con.on("error", function (err) {
+    console.error("Unexpected database error:", err.message);
+    console.log("Attempting to reconnect...");
+    connectToDatabase();
+  });
+}
+
+connectToDatabase();
 
 module.exports = con;
